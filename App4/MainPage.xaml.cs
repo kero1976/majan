8528f2c21d5499a2e0c8a78c2e992712451da4bc
kero1976 <kero1976@outlook.jp>
2018-03-18 09:36:23 +0900

    プロジェクト ファイルを追加します。

---
+++ App4/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// 空白ページの項目テンプレートについては、https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x411 を参照してください
+
+namespace App4
+{
+    /// <summary>
+    /// それ自体で使用できる空白ページまたはフレーム内に移動できる空白ページ。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        Person p1;
+        public MainPage()
+        {
+            this.InitializeComponent();
+            p1 = new Person(1, 25000);
+            //Person p2 = new Person(2, 25000);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            debugText.Text = p1.ToString();
+        }
+    }
+}
---
+++ App4/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App4
+{
+    public class Person
+    {
+        int kaze;
+        int tensu;
+
+        public Person(int _kaze, int _tensu)
+        {
+            this.kaze = _kaze;
+            this.tensu = _tensu;
+        }
+
+        public override string ToString()
+        {
+            return $"風{this.kaze}";
+        }
+    }
+}
